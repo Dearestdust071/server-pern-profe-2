9dd7a8fc48ea4d1328f8ba0b0ab6969724a9504a
Dust <dearestdust071@gmail.com>
2025-07-23 13:40:51 -0600

    Feat: Agregada funcion en data/index para limpiar bd

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,11 +1,22 @@
-import { Request, Response } from "express";
-import Product from "../models/Producto.mo";
+import db from '../config/db';
+import Product from '../models/Producto.mo';
 
+async function clearDatabase() {
+  try {
+    await db.authenticate();
+    await Product.destroy({ where: {} }); // Borra todos los productos
+    console.log('Base de datos limpiada');
+    process.exit(0);
+  } catch (error) {
+    console.error('Error al limpiar la base de datos', error);
+    process.exit(1);
+  }
+}
 
-const args = process.argv
-
-
-
-if(args.includes("--clear")){
-
+const args = process.argv.slice(2);
+if (args.includes('--clear')) {
+  clearDatabase();
+} else {
+  console.log('No se especificó --clear');
+  process.exit(0);
 }
