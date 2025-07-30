9acf54466290735d810e376ab6783e78c921e75d
Dust <dearestdust071@gmail.com>
2025-07-29 19:45:10 -0600

    feat: Un trucazo B)

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import db from "./config/db";
 import colors from "colors";
 import swaggerSpec, { swaggerUiOptions } from "./config/swagger";
 import swaggerUi from 'swagger-ui-express';
-
+// Que trucazo no? DB_URL=postgresql://postgres2_sjnc_user:IqHk6pryPXXYcWkFS2hCtmlwrO1H7hEf@dpg-d1v8bva4d50c73dasct0-a.oregon-postgres.render.com/postgres2_sjnc?ssl=true |  PORT=4000
 // Aqui o instancio la bd porque si no creo que no jala     
 export async function connectionDB() {
     try {
